aa06a9f500b6d7bf6ada118c26038a487dc7d622
Dwight Nodolf <dnodolf@gmail.com>
2020-05-11 04:19:00 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailAddressParser
   
    def parse
      split_mail = @email_addresses.split(", " || " ")
-     split_mail.collect{ |address| address.uniq }
+     split_mail.collect.uniq{ |address| address }
      
    end
    
